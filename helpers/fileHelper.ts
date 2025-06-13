d37ccabfcb033485b8c7fce959da3314382707d0
Phantom Cracker <popa.vlad94@gmail.com>
2025-06-13 10:47:08 +0300

    2.1: Merge CSV file with the new data

--- helpers/fileHelper.ts
+++ helpers/fileHelper.ts
@@ -1,26 +1,108 @@
 import fs, { read } from 'fs';
 import csvParser from 'csv-parser';
+import { createObjectCsvWriter } from 'csv-writer';
 
-interface DomainRow {
-    domain?: string;
+export interface CsvRow {
+    domain: string,
+    company_commercial_name?: string,
+    company_legal_name?: string,
+    company_all_available_names?: string[],
+    physical_addresses?: string | string[],
+    social_media_links?: string | string[],
+    phone_numbers?: string | string[]
 }
 
-function readDomainsFromCSV(filePath: string): Promise<Set<string>> {
-    return new Promise ((resolve, reject) => {
-        const domainsList = new Set<string>();
+const newFields: string[] = ['physical_addresses', 'phone_numbers', 'social_media_links'];
+
+function readFullRowsFromCSV(filePath: string): Promise<CsvRow[]> {
+    return new Promise((resolve, reject) => {
+        const rows: CsvRow[] = [];
 
         fs.createReadStream(filePath)
             .pipe(csvParser())
-            .on('data', (row: DomainRow) => {
+            .on('data', (row: any) => {
                 if (row.domain) {
-                    domainsList.add(row.domain.trim());
+                    rows.push({
+                        domain: row.domain.trim(),
+                        company_commercial_name: row.company_commercial_name?.trim(),
+                        company_legal_name: row.company_legal_name?.trim(),
+                        company_all_available_names: row.company_all_available_names ? 
+                            row.company_all_available_names.split('|').map((companyName: string) => companyName.trim()) : []
+                    });
                 }
             })
-            .on('end', () => resolve(domainsList))
-            .on('error', reject)
-    })
+            .on('end', () => resolve(rows))
+            .on('error', reject);
+    });
+}
+
+function mergeData(existingData: CsvRow[], updatesData: CsvRow[]): CsvRow[] {
+    const domainsRowMap = new Map<string, CsvRow>();
+
+    for (const oldRow of existingData) {
+        domainsRowMap.set(oldRow.domain.toLowerCase(), { ...oldRow });
+    }
+
+    for (const newRow of updatesData) {
+        const domain = newRow.domain.toLowerCase();
+
+        if (!domainsRowMap.has(domain)) continue;
+
+        const row = domainsRowMap.get(domain);
+        const mergedRow = { ...row };
+
+        for (const field of newFields) {
+            if (field in newRow && newRow[field] !== undefined && newRow !== null && (!row[field] || row[field] === '')) {
+                mergedRow[field] = newRow[field];
+            } 
+        }
+        domainsRowMap.set(domain, mergedRow);
+    }
+
+    return Array.from(domainsRowMap.values());
+}
+
+function getAllHeaders(rowsList: CsvRow[]): string[] {
+    const headerSet = new Set<string>();
+    
+    for (const row of rowsList) {
+        Object.keys(row).forEach(key => headerSet.add(key));
+    }
+    newFields.forEach(field => headerSet.add(field));
+
+    return Array.from(headerSet);
+}
+
+async function writeCsv(filePath: string, data: CsvRow[]): Promise<void> {
+    const headersList = getAllHeaders(data);
+    const csvWriter = createObjectCsvWriter({
+        path: filePath,
+        header: headersList.map(header => ({ id: header, title: header }))
+    });
+
+    await csvWriter.writeRecords(
+        data.map(row => {
+            const newRow: any = { ...row };
+
+            for (const key of headersList) {
+                if (Array.isArray(newRow[key])) {
+                    newRow[key] = newRow[key].join("|");
+                }
+            }
+            
+            return newRow;
+        })
+    )
+}
+
+// i will use output path too so i don't alter the original document
+async function mergeCsvFiles(inputPath: string, newData: CsvRow[], outputPath: string): Promise<void> {
+    const oldRows = await readFullRowsFromCSV(inputPath);
+    const mergedRows = mergeData(oldRows, newData);
+    await writeCsv(outputPath, mergedRows);
 }
 
 export default {
-    readDomainsFromCSV: readDomainsFromCSV
+    readFullRowsFromCSV: readFullRowsFromCSV,
+    mergeCsvFiles: mergeCsvFiles
 }
--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import pMap from 'p-map';
 
 import urlHelper from './helpers/urlHelper';
 import fileHelper from './helpers/fileHelper';
+import { CsvRow } from './helpers/fileHelper';
 import collectDataHelper from './helpers/collectDataHelper';
 import playwrightHelper from './helpers/playwrightHelper';
 
@@ -15,8 +16,11 @@ import playwrightHelper from './helpers/playwrightHelper';
         socialMediaCrawled: number,
         physicalAddressesCrawled: number
     }
-    const filePath: string = './data/sample-websites.csv';
-    const domainsList = await fileHelper.readDomainsFromCSV(filePath);
+
+    const filePathToDomains: string = './data/sample-websites.csv';
+    const filePathToCompanies: string = './data/sample-websites-company-names.csv';
+    const filePathToMergedDocument: string = './data/finished.csv'
+    const domainsList = await fileHelper.readFullRowsFromCSV(filePathToDomains);
     // const domainsList = ['https://timent.com/', 'kansaslimousin.org', 'kkcger.com'];
     const browser: Browser = await chromium.launch({ headless: true });
     playwrightHelper.shutdownOnSignals(browser);
@@ -27,12 +31,15 @@ import playwrightHelper from './helpers/playwrightHelper';
         socialMediaCrawled: 0,
         physicalAddressesCrawled: 0
     }
+    let newData: CsvRow[] = [];
 
     async function crawlDomain(domain: string): Promise<void> {
         // need the page in order to close correctly (even on error) with finally
         let page: Page | undefined;
+        let newRow: CsvRow = { domain: domain };
 
         // append https because 'new URL' fails otherwise as it is not a valid URL
+        // TODO: try http and www variations too
         if (!/^https?:\/\//i.test(domain)) {
             domain = 'https://' + domain;
         }
@@ -57,14 +64,18 @@ import playwrightHelper from './helpers/playwrightHelper';
 
             const physicalAddressesList = await collectDataHelper.getPhysicalAddress(page, domain);
             analystData.physicalAddressesCrawled += physicalAddressesList.length;
-            console.log("Physical addresses for " + domain + " are: " + physicalAddressesList);
             
             const domainLinks = await urlHelper.getDomainLinks(page, domain, origin);
             analystData.phonesCrawled += domainLinks.phoneNumbersList.length;
             analystData.socialMediaCrawled += domainLinks.socialMediaLinks.length;
 
             console.log(domainLinks);
-            console.log("Domains crawled: ", analystData.websitesCrawled);
+            // console.log("Domains crawled: ", analystData.websitesCrawled);
+            
+            newRow.physical_addresses = physicalAddressesList;
+            newRow.social_media_links = domainLinks.socialMediaLinks;
+            newRow.phone_numbers = domainLinks.phoneNumbersList;
+            newData.push(newRow);
         } catch(error) {
             console.error("Error processing the domain " + domain + " with the following error: " + error);
         } finally {
@@ -78,13 +89,13 @@ import playwrightHelper from './helpers/playwrightHelper';
         }
     }
 
-    await pMap(domainsList, crawlDomain, { concurrency: 5 });
+    await pMap(domainsList.map(row => row.domain), crawlDomain, { concurrency: 5 });
+    playwrightHelper.safeShutdown(browser);
+    await fileHelper.mergeCsvFiles(filePathToCompanies, newData, filePathToMergedDocument);
 
     console.log("Crawled a total of " + analystData.websitesCrawled + " websites");
     console.log("Additional details: ");
     console.log("Crawled a total of " + analystData.physicalAddressesCrawled + " physical addresses");
     console.log("Crawled a total of " + analystData.phonesCrawled + " phone numbers");
     console.log("Crawled a total of " + analystData.socialMediaCrawled + " social media links");
-    
-    playwrightHelper.safeShutdown(browser);
 })();
